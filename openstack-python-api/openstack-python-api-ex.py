7e05a2a00fdabfa11a230b9b7d8a9a770078af33
Chris Geroux <chris.m.geroux@gmail.com>
2016-05-04 19:01:04 +0000

    Initial commit

---
+++ openstack-python-api/openstack-python-api-ex.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from os import environ as env
+from novaclient.client as nvclient
+from __future__ import print_function
+
+def main():
+  
+  #create the nova client
+  APIVersion="2.0"
+  nova=nvclient.Client(APIVersion
+    ,auth_url=env['OS_AUTH_URL']#URL to connect to
+    ,username=env['OS_USERNAME']#CC Cloud user name
+    ,api_key=env['OS_PASSWORD']#CC cloud password
+    ,project_id=env['OS_TENANT_NAME']#CC cloud project name
+    ,region_name=env['OS_REGION_NAME']#CC cloud region name
+    )
+    
+  #get a list of current servers
+  servers=nova.servers.list()
+  
+  print()
+  print("dir(servers)=",dir(servers[0]))
+  print()
+  print("nova.servers.list.__doc__=",nova.servers.list.__doc__)
+  print()
+  
+  #print out a list of the current VM names
+  for server in servers:
+    print server.name
+
+if __name__=="__main__":
+  main()
---
+++ spark/wordcount/wordcount.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark import SparkContext
+
+
+if __name__ == "__main__":
+  
+  #check that we have expected number of arguments
+  if len(sys.argv) != 3:
+    print("Got the following arguments:"+str(sys.argv))
+    print("Usage: wordcount <input-file> <output-dir>", file=sys.stderr)
+    exit(-1)
+  
+  #get the spark context
+  sc = SparkContext(appName="PythonWordCount")
+  
+  #get input and output files
+  inputFile=sys.argv[1]
+  outputFile=sys.argv[2]
+  
+  #read in input
+  lines = sc.textFile(inputFile)
+  
+  counts = lines.flatMap(lambda x: x.split(' ')) \
+                .map(lambda x: (x, 1)) \
+                .reduceByKey(add)#no exceptions here
+  counts.saveAsTextFile(sys.argv[2])#works
+  
+  #convert to a list
+  #output = counts.collect()
+  
+  #loop over the list and print out to std out
+  #for (word, count) in output:
+  #    print("%s: %i" % (word, count))
+  #
+  sc.stop()
